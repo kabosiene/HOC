3317cd10bd942e6d5f1109bf80cf800859c67643
Kristina <kristina.kabosiene@gmail.com>
2017-10-18 20:04:52 +0300

    add sarasai lesson

---
+++ js/sarasai.js
@@ -0,0 +1,153 @@
+Blockly.Blocks['tekstas1'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("1. Eilutė");
+    this.setOutput(true, null);
+    this.setColour(160);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['tekstas1'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  //var code = '("Lietuvos Taryba, kaip vienintelė lietuvių tautos atstovybė,")';
+  var code = "1";
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.Blocks['tekstas2'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("2. Eilutė");
+    this.setOutput(true, null);
+    this.setColour(160);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['tekstas2'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  //var code = 'remdamos pripažintąja tautų apsisprendimo teise ir lietuvių Vilniaus konferencijos nutarimu rugsėjo mėn. 18-23 d. 1917 metais,';
+  var code = "2";
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.Blocks['tekstas3'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("3. Eilutė");
+    this.setOutput(true, null);
+    this.setColour(160);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['tekstas3'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  //var code = 'skelbia atstatanti nepriklausomą demokratiniais pamatais sutvarkytą Lietuvos valstybę su sostine Vilniuje';
+  var code = "3";
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.Blocks['tekstas4'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("4. Eilutė");
+    this.setOutput(true, null);
+    this.setColour(160);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['tekstas4'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  //var code = '"ir tą valstybę atskirianti nuo visų valstybinių ryšių, kurie yra buvę su kitomis tautomis."';
+  var code = "4";
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.Blocks['tekstas5'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("5. Eilutė");
+    this.setOutput(true, null);
+    this.setColour(160);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['tekstas5'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  //var code = 'Drauge Lietuvos Taryba pareiškia,';
+  var code = "5";
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.Blocks['tekstas6'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("6. Eilutė");
+    this.setOutput(true, null);
+    this.setColour(160);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['tekstas6'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  //var code = 'kad Lietuvos valstybės pamatus ir jos santykius su kitomis valstybėmis privalo galutinai nustatyti kiek galima greičiau sušauktas steigiamasis seimas,';
+  var code = "6";
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.Blocks['tekstas7'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("7. Eilutė");
+    this.setOutput(true, null);
+    this.setColour(160);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['tekstas7'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'demokratiniu būdu visų jos gyventojų išrinktas.';
+  var code = "7";
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.Blocks['tekstas8'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("8. Eilutė");
+    this.setOutput(true, null);
+    this.setColour(160);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['tekstas8'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  //var code = 'Lietuvos Taryba pranešdama apie tai ....... vyriausybei, ';
+  var code = "8";
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.Blocks['tekstas9'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("9. Eilutė");
+    this.setOutput(true, null);
+    this.setColour(160);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['tekstas9'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  //var code = 'prašo pripažinti nepriklausomą Lietuvos valstybę.';
+  var code = "9";
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
