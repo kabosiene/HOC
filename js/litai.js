864f407fb975554349c20bd95caeb69a30a7ab20
Kristina <kristina.kabosiene@gmail.com>
2017-10-24 11:11:01 +0300

    add lesson fix valiutos
    still need to fix eventlistener anonymous function

--- js/litai.js
+++ js/litai.js
@@ -1,14 +1,19 @@
-    var my_canvas = document.getElementById("canvas");
+
+        var my_canvas = document.getElementById("canvas");
     var ctx = my_canvas.getContext("2d");
-    my_canvas.addEventListener('click', clickReporterPirkti, false);
     var button = new Image();
     var button_euras = new Image();
     var buy = new Image();
     var sakotis = new Image();
-
+    my_canvas.addEventListener('click', clickReporterPirkti, false);
     var eurasKainuoja = 3.45;
-    var turimLitų = 0;
-    var turimEurų = 0;
+    // var turimLitų = 0;
+    // var turimEurų = 0;
+    var litas;
+    var euras;
+    var paspausta;
+    var callAgain;
+
 
 
     button.src = "../test_img/download.jpg";
@@ -38,72 +43,94 @@
         var code ="";
 
         if (dropdown_name == "EURO") {
-            my_canvas.addEventListener('click', clickReporterEuras, false);
+            //var a =  my_canvas.addEventListener('click', clickReporterEuras, false);
+          // my_canvas.addEventListener('click', clickReporterEuras, false);
+           code = "paspausta == 'eurai'";
 
         } else if (dropdown_name == "LITAS") {
-            my_canvas.addEventListener('click', clickReporterLitas, false);
+            //var b = my_canvas.addEventListener('click', clickReporterLitas, false);
+          // my_canvas.addEventListener('click', clickReporterLitas, false);
+           //alert(paspausta);
+           code = "paspausta == 'litai'";
 }
-        // else if (dropdown_name == "PIRKTI") {
-        //     my_canvas.addEventListener('click', clickReporterPirkti, false);
-        // }
-        // TODO: Change ORDER_NONE to the correct strength.
         return [code, Blockly.JavaScript.ORDER_NONE];
     };
-
-    Blockly.Blocks['keisti_valiuta'] = {
-        init: function() {
-            this.appendValueInput("padidink")
-                .setCheck(null)
-                .appendField("padidink")
-                .appendField(new Blockly.FieldDropdown([
-                    ["litai", "LITAS"],
-                    ["eurai", "EURO"]
-                ]), "pinigai");
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(20);
-            this.setTooltip("");
-            this.setHelpUrl("");
-        }
-    };
-
-    Blockly.JavaScript['keisti_valiuta'] = function(block) {
-        var dropdown_pinigai = block.getFieldValue('pinigai');
-        var value_padidink = Blockly.JavaScript.valueToCode(block, 'padidink', Blockly.JavaScript.ORDER_ATOMIC);
-
-        // TODO: Assemble JavaScript into code variable.
-        if(dropdown_pinigai == "EURO"){
-              turimEurų += value_padidink;
-        }
-        if (dropdown_pinigai == "LITAS"){
-             turimLitų = turimEurų * value_padidink ;
-        }
-        
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        draw_elements();
-        var code =  dropdown_pinigai + value_padidink+";\n";
-        return code;
-    };
-    Blockly.Blocks['kintamasis'] = {
-        init: function() {
-            this.appendDummyInput()
-                .appendField(new Blockly.FieldNumber(0), "KINTAMASIS");
-            this.setOutput(true, null);
-            this.setColour(230);
-            this.setTooltip("");
-            this.setHelpUrl("");
-        }
-    }
-
-    Blockly.JavaScript['kintamasis'] = function(block) {
-        var number_kintamasis = block.getFieldValue('KINTAMASIS');
-        // TODO: Assemble JavaScript into code variable.
-        var code = number_kintamasis;
-        // TODO: Change ORDER_NONE to the correct strength.
-        return [code, Blockly.JavaScript.ORDER_NONE];
-    };
+    //*****************************************************************************
+    Blockly.Blocks['change'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("prie")
+        .appendField(new Blockly.FieldDropdown([["litai","litas"], ["eurai","euras"]]), "NAME")
+        .appendField("pridėk")
+        .appendField(new Blockly.FieldNumber(0), "NUMBER");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(330);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['change'] = function(block) {
+  var dropdown_name = block.getFieldValue('NAME');
+  var number_name = block.getFieldValue('NUMBER');
+  // TODO: Assemble JavaScript into code variable.
+  var code = ''+dropdown_name+' += '+number_name+';\n';
+  return code;
+};
+Blockly.Blocks['set'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["litai","litas"], ["eurai","euras"]]), "NAME")
+        .appendField("=")
+        .appendField(new Blockly.FieldNumber(0, 0), "NUMBER");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(330);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['set'] = function(block) {
+  var dropdown_name = block.getFieldValue('NAME');
+  var number_name = block.getFieldValue('NUMBER');
+  // TODO: Assemble JavaScript into code variable.
+    var code = ''+dropdown_name+' = '+number_name+';\n';
+  return code;
+};
 
 
+
+Blockly.Blocks['ifas'] = {
+  init: function() {
+    this.appendValueInput("if_reiksme")
+        .setCheck(Boolean)
+        .appendField("if");
+    this.appendStatementInput("do_reiksme")
+        .setCheck(null)
+        .appendField("do");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['ifas'] = function(block) {
+  var value_if_reiksme = Blockly.JavaScript.valueToCode(block, 'if_reiksme', Blockly.JavaScript.ATOMIC);
+  var statements_do_reiksme = Blockly.JavaScript.statementToCode(block, 'do_reiksme');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'callAgain(function(){if('+value_if_reiksme+'){\n'+statements_do_reiksme+'}});\n';
+ // var code2 = 'if('+value_if_reiksme+'){\n'+statements_do_reiksme+'}\n;'
+  return code;
+};
+    //*****************************************************************************
+
+    function callAgain(if_body){
+     
+      clicked = function(e){clickReporter(e,if_body)};
+        my_canvas.addEventListener('click', clicked);
+
+};
 
     button.onload = function() {
         draw(button, 10, 10);
@@ -124,42 +151,20 @@
     }
 
     function draw_elements() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         draw(button, 10, 10);
         draw(buy, 250, 200);
         draw(button_euras, 10, 120);
         draw(sakotis, 300, 50);
         ctx.strokeText("Kaina: 30 Litų", 300, 50);
-        ctx.strokeText("Eurai: " + turimEurų, 10, 120);
-        ctx.strokeText("Surinkta suma: " + turimLitų, 10, 10);
+        ctx.strokeText("Eurai: " + euras, 10, 120);
+        ctx.strokeText("Litai: " + litas, 10, 10);
     }
 
-    function exchangeLitai() {
-
-        // turimEurų += 1;
-        // turimLitų = turimEurų * eurasKainuoja;
-        // ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // draw_elements();
-    }
-
-    function clickReporterEuras(e) { /// assign event to some variable
+   function clickReporter(e, if_body) { /// assign event to some variable
 
         /// adjust mouse click position to be relative to canvas:
-        var rect = this.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-
-        //button_euras
-        if (x >= 10 && x <= (10 + button_euras.width) &&
-            y >= 120 && y <= (120 + button_euras.height)) {
-            exchangeLitai();
-        }
-
-    }
-
-    function clickReporterLitas(e) { /// assign event to some variable
-
-        /// adjust mouse click position to be relative to canvas:
-        var rect = this.getBoundingClientRect(),
+        var rect = canvas.getBoundingClientRect(),
             x = e.clientX - rect.left,
             y = e.clientY - rect.top;
 
@@ -169,28 +174,33 @@
             y >= 10 && y <= (10 + button.height)) {
 
             //exchangeLitai();
+        paspausta = "litai";
+        if_body();
+        draw_elements();
+
         }
-    }
+        //button_euras
+         if (x >= 10 && x <= (10 + button_euras.width) &&
+            y >= 120 && y <= (120 + button_euras.height)) {
 
-    function clickReporterPirkti(e) { /// assign event to some variable
-
-        /// adjust mouse click position to be relative to canvas:
-        var rect = this.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-
+           // exchangeLitai();
+         paspausta = "eurai";
+          if_body();
+          draw_elements();
+        }
+        //buy
         if (x >= 250 && x <= (250 + buy.width) &&
             y >= 200 && y <= (200 + buy.height)) {
 
 
-            if (turimLitų > 30) {
+            if (litas > 30) {
                 ctx.clearRect(0, 0, 10, 10);
                 ctx.strokeText("NUSIPIRKAI", 200, 100);
+                 setTimeout(function(){$('#nextModal').modal('show');},1000);
             } else {
 
-                turimLitų = 0;
-                turimEurų = 0;
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                litas = 0;
+                euras = 0;
                 draw_elements();
                 ctx.strokeText("TRUKSTA", 200, 100);
             }
@@ -199,7 +209,34 @@
 
 
 
-    // function clickReporter(e) { /// assign event to some variable
+
+
+    // function exchangeLitai() {
+
+    //     // turimEurų += 1;
+    //     // turimLitų = turimEurų * eurasKainuoja;
+    //     // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //     // draw_elements();
+    // }
+
+    // function clickReporterEuras(e) { /// assign event to some variable
+
+    //     /// adjust mouse click position to be relative to canvas:
+    //     var rect = this.getBoundingClientRect(),
+    //         x = e.clientX - rect.left,
+    //         y = e.clientY - rect.top;
+
+    //     //button_euras
+    //     if (x >= 10 && x <= (10 + button_euras.width) &&
+    //         y >= 120 && y <= (120 + button_euras.height)) {
+    //   //     euras = litas / 3.45;
+    //   // litas = 0;
+    //   //  draw_elements();
+    //     }
+
+    // }
+
+    // function clickReporterLitas(e) { /// assign event to some variable
 
     //     /// adjust mouse click position to be relative to canvas:
     //     var rect = this.getBoundingClientRect(),
@@ -212,28 +249,37 @@
     //         y >= 10 && y <= (10 + button.height)) {
 
     //         //exchangeLitai();
-    //     }
-    //     //button_euras
-    //      if (x >= 10 && x <= (10 + button_euras.width) &&
-    //         y >= 120 && y <= (120 + button_euras.height)) {
-
-    //         exchangeLitai();
-    //     }
-    //     //buy
-    //     if (x >= 250 && x <= (250 + buy.width) &&
-    //         y >= 200 && y <= (200 + buy.height)) {
-
-
-    //         if (turimLitų > 30) {
-    //             ctx.clearRect(0, 0, 10, 10);
-    //             ctx.strokeText("NUSIPIRKAI", 200, 100);
-    //         } else {
-
-    //             turimLitų = 0;
-    //             turimEurų = 0;
-    //             ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //             draw_elements();
-    //             ctx.strokeText("TRUKSTA", 200, 100);
-    //         }
+    //      // litas += 1;
+    //      // draw_elements();
+    //        //                  var blocks = workspace.getAllBlocks();
+    //        // alert (blocks);
     //     }
     // }
+
+    function clickReporterPirkti(e) { /// assign event to some variable
+
+        /// adjust mouse click position to be relative to canvas:
+        var rect = this.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        if (x >= 250 && x <= (250 + buy.width) &&
+            y >= 200 && y <= (200 + buy.height)) {
+
+
+            if (litas >= 30) {
+                ctx.clearRect(0, 0, 10, 10);
+                ctx.strokeText("NUSIPIRKAI", 200, 100);
+                setTimeout(function(){$('#nextModal').modal('show');},1000);
+            } else {
+
+                litas = 0;
+                euras = 0;
+                draw_elements();
+                ctx.strokeText("TRUKSTA", 200, 100);
+            }
+        }
+    }
+
+
+
