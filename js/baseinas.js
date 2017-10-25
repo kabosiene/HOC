cd7144cd808401d183d6ef6aa3fbed6176f28ad3
Kristina <kristina.kabosiene@gmail.com>
2017-10-25 18:07:32 +0300

    add baseinas fixup

--- js/baseinas.js
+++ js/baseinas.js
@@ -22,7 +22,7 @@ Blockly.Blocks['apsisuk'] = {
     init: function() {
         this.setColour(230);
         this.appendDummyInput()
-            .appendField('Apsukti plaukiką');
+            .appendField('apsukti plaukiką');
         this.setTooltip('Blokelis prideda kintamąjį, nurodantį apsisukti.');
         this.setNextStatement(true);
         this.setPreviousStatement(true);
@@ -35,60 +35,50 @@ Blockly.JavaScript['apsisuk'] = function(block) {
 };
 
 
-// Blockly.Blocks['keisti'] = {
-//     init: function() {
-//         this.appendDummyInput()
-//             .appendField("keisti kostiumą");
-//         this.setColour(230);
-//         this.setTooltip('Blokelis prideda plaukimo animaciją.');
-//         this.setNextStatement(true);
-//         this.setPreviousStatement(true);
-//     }
-// };
-
-// Blockly.JavaScript['keisti'] = function(block) {
-//       var code = "costume = 'swim_anim';\n";
-//       add_costume = true;
-//     return code;
-// };
 Blockly.Blocks['keisti'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("keisti kostiumą į ")
-        .appendField(new Blockly.FieldDropdown([["swim","swim"], ["swim2","swim2"]]), "kostiumas");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(65);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+    init: function() {
+        this.appendDummyInput()
+            .appendField("keisti kostiumą į ")
+            .appendField(new Blockly.FieldDropdown([
+                ["swim", "add_costume_swim"],
+                ["swim2", "add_costume_swim2"]
+            ]), "kostiumas");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(65);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
 };
 Blockly.JavaScript['keisti'] = function(block) {
-  var dropdown_kostiumas = block.getFieldValue('kostiumas');
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'costume = '+dropdown_kostiumas+';\n';
-  add_costume = true;
-  return code;
+    var dropdown_kostiumas = block.getFieldValue('kostiumas');
+    // TODO: Assemble JavaScript into code variable.
+    var code = 'costume = ' + dropdown_kostiumas + ';\n';
+    if (dropdown_kostiumas == 'add_costume_swim') {add_costume_swim = true}
+    if (dropdown_kostiumas == 'add_costume_swim2') {add_costume_swim2 = true}
+   // dropdown_kostiumas = true;
+    console.log(dropdown_kostiumas);
+    return code;
 };
 Blockly.Blocks['set'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("laukti")
-        .appendField("= ")
-        .appendField(new Blockly.FieldNumber(0, 0), "NUMBER")
-         .appendField("s");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(330);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+    init: function() {
+        this.appendDummyInput()
+            .appendField("laukti")
+            .appendField("= ")
+            .appendField(new Blockly.FieldNumber(0, 0), "NUMBER")
+            .appendField("s");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(330);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
 };
 Blockly.JavaScript['set'] = function(block) {
-  var number_name = block.getFieldValue('NUMBER');
-  // TODO: Assemble JavaScript into code variable.
-    var code = 'document.getElementById("test1").style.animationDuration = "'+number_name+'s";\n';
-  return code;
+    var number_name = block.getFieldValue('NUMBER');
+    // TODO: Assemble JavaScript into code variable.
+    var code = 'document.getElementById("test1").style.animationDuration = "' + number_name + 's";\n';
+    return code;
 };
 
 
@@ -107,9 +97,9 @@ Blockly.Blocks['keisti_greiti'] = {
 
 Blockly.JavaScript['keisti_greiti'] = function(block) {
     var value_padidink = Blockly.JavaScript.valueToCode(block, 'padidink', Blockly.JavaScript.ORDER_ATOMIC);
-    add_speed = value_padidink * 0.01;
+   // add_speed = value_padidink * 0.01;
     // TODO: Assemble JavaScript into code variable.
-    var code = 'speed += ' + value_padidink * 0.01 + ';\n';
+    var code = 'add_speed += ' + value_padidink * 0.01 + ';\n';
     return code;
 };
 
@@ -132,42 +122,147 @@ Blockly.JavaScript['on_click'] = function(block) {
 
 
 Blockly.Blocks['function'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("komanda: ")
-        .appendField(new Blockly.FieldTextInput("plaukti"), "SWIM");
-    this.appendStatementInput("FUNCTION_BODY")
-        .setCheck(null);
-    this.setColour(290);
- this.setTooltip("Funkcija");
- this.setHelpUrl("");
-  }
+    init: function() {
+        this.appendDummyInput()
+            .appendField("komanda: ")
+            .appendField(new Blockly.FieldTextInput("plaukti"), "SWIM")
+            .appendField(new Blockly.FieldVariable("greitis"), "speed_var");
+        this.appendStatementInput("body")
+            .setCheck(null);
+        this.setColour(290);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
 };
-
 Blockly.JavaScript['function'] = function(block) {
-  var text_swim = block.getFieldValue('SWIM');
-  var statements_body = Blockly.JavaScript.statementToCode(block, 'FUNCTION_BODY');
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'function plaukti() {\n'+statements_body+'};\n';
-  swim_do_f = true;
-  return code;
+    var text_swim = block.getFieldValue('SWIM');
+    var variable_speed_var = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('speed_var'), Blockly.Variables.NAME_TYPE);
+    var statements_body = Blockly.JavaScript.statementToCode(block, 'body');
+    // TODO: Assemble JavaScript into code variable.
+    var code = 'function plaukti(greitis) {\n' + statements_body + '};\n';
+    swim_do_f = true;
+    return code;
 };
 
 Blockly.Blocks['function_call'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("plaukti(")
+            .appendField(new Blockly.FieldVariable("greitis"), "NAME")
+            .appendField(")");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(290);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+Blockly.JavaScript['function_call'] = function(block) {
+    var variable_name = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Variables.NAME_TYPE);
+    // TODO: Assemble JavaScript into code variable.
+    var code = 'plaukti(greitis);\n';
+    swim_do = true;
+    return code;
+};
+
+Blockly.Blocks['plaukti'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("plaukti");
+        this.setColour(230);
+        this.setTooltip('Blokelis prideda kintamąjį, nurodantį plaukti.');
+        this.setNextStatement(true);
+        this.setPreviousStatement(true);
+    }
+};
+
+Blockly.JavaScript['plaukti'] = function(block) {
+    var code = 'swim_do = true;\n ';
+    swim_do = true;
+    return code;
+};
+Blockly.Blocks['anim_speed'] = {
+    init: function() {
+        this.appendValueInput("ANIM_SPEED")
+            .setCheck(null)
+            .appendField("animacijos greitis = ");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(330);
+        this.setTooltip("Plaukimo animacijos greitis");
+        this.setHelpUrl("");
+    }
+};
+Blockly.JavaScript['anim_speed'] = function(block) {
+    var value_anim_speed = Blockly.JavaScript.valueToCode(block, 'ANIM_SPEED', Blockly.JavaScript.ORDER_NONE);
+    // TODO: Assemble JavaScript into code variable.
+    //var code = 'document.getElementById("test1").style.animationDuration = ""+(0.1/' + value_anim_speed + ')+"s;\n';
+    //var code = 'document.getElementById("test1").style.animationDuration = "' + value_anim_speed + 's";\n';
+var code = 'anim_speed = '+value_anim_speed+';\n'
+animation_speed = true;
+    return code;
+};
+Blockly.Blocks['var_speed'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldVariable("greitis"), "var_speed");
+        this.setOutput(true, null);
+        this.setColour(330);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+Blockly.JavaScript['var_speed'] = function(block) {
+    var variable_var_speed = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('var_speed'), Blockly.Variables.NAME_TYPE);
+    // TODO: Assemble JavaScript into code variable.
+
+    var code = speed;
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.Blocks['function_x'] = {
   init: function() {
+    this.appendValueInput("plaukti_greitis")
+        .setCheck(null)
+        .appendField("funkcija:")
+        .appendField("plaukti(");
     this.appendDummyInput()
-        .appendField("plaukti");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
+        .appendField(");");
+    this.appendStatementInput("body")
+        .setCheck(null);
+    this.setInputsInline(true);
     this.setColour(290);
- this.setTooltip("Funkcijos kvietimas");
+ this.setTooltip("");
  this.setHelpUrl("");
   }
 };
-
-Blockly.JavaScript['function_call'] = function(block) {
+Blockly.JavaScript['function_x'] = function(block) {
+  var value_plaukti_greitis = Blockly.JavaScript.valueToCode(block, 'plaukti_greitis', Blockly.JavaScript.ORDER_ATOMIC);
+  var statements_body = Blockly.JavaScript.statementToCode(block, 'body');
   // TODO: Assemble JavaScript into code variable.
-  var code = 'plaukti();\n';
-   swim_do = true;
+     var code = 'function plaukti('+value_plaukti_greitis+') {\n' + statements_body + '};\n';
+    swim_do_f = true;
+  return code;
+};
+Blockly.Blocks['function_call_x'] = {
+  init: function() {
+    this.appendValueInput("f_plaukti")
+        .setCheck(null)
+        .appendField("plaukti(");
+    this.appendDummyInput()
+        .appendField(");");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(290);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['function_call_x'] = function(block) {
+  var value_f_plaukti = Blockly.JavaScript.valueToCode(block, 'f_plaukti', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+      var code = 'plaukti('+value_f_plaukti+');\n';
+    swim_do = true;
   return code;
 };
