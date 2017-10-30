2217749fdf092ed4bd574b37a499b57a9e53d2a3
Kristina <kristina.kabosiene@gmail.com>
2017-10-30 16:55:48 +0200

    add sample

---
+++ js/mazylis.js
@@ -0,0 +1,17 @@
+// mazylis
+// var x, list, list_x;
+
+
+// list = '18 aa;19bb;18 dd'.split(';');
+// x = contains(list,18)
+// window.alert(x);
+
+// function contains(a, obj) {
+// var list_x = [];
+//     for (var i = 0; i < a.length; i++) {
+//         if (a[i].includes(obj)) {
+//             list_x.push(i);
+//         }
+//     }
+//     return list_x;
+// }
