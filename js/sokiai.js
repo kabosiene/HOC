c9473d067103a8a6f528e9318a2019dc904a11a1
Kristina <kristina.kabosiene@gmail.com>
2017-11-02 15:41:04 +0200

    add l7 sample dance, music, blocks

--- js/sokiai.js
+++ js/sokiai.js
@@ -1,3 +1,8 @@
+var anim_speed = 2;
+      var audio = new Audio('../Kepurine.mp3');
+      audio.pause();
+      
+audio.currentTime = 0;
 Blockly.Blocks['anim_speed'] = {
     init: function() {
         this.appendValueInput("ANIM_SPEED")
@@ -15,7 +20,67 @@ Blockly.JavaScript['anim_speed'] = function(block) {
     // TODO: Assemble JavaScript into code variable.
     //var code = 'document.getElementById("test1").style.animationDuration = ""+(0.1/' + value_anim_speed + ')+"s;\n';
     //var code = 'document.getElementById("test1").style.animationDuration = "' + value_anim_speed + 's";\n';
-var code = 'anim_speed = '+value_anim_speed+';\n'
-//anim_speed = true;
+    var code = 'anim_speed = ' + value_anim_speed + ';\n';
+    code += 'aSpeed(anim_speed);';
+    //anim_speed = true;
     return code;
+};
+
+Blockly.Blocks['wait_seconds'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("wait")
+            .appendField(new Blockly.FieldTextInput("1"), "SECONDS")
+            .appendField("seconds");
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setColour(120);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+/**
+ * Generator for wait block creates call to new method
+ * <code>waitForSeconds()</code>.
+ */
+Blockly.JavaScript['wait_seconds'] = function(block) {
+    var seconds = Number(block.getFieldValue('SECONDS'));
+    var code = 'waitForSeconds(' + seconds + ');\n';
+    return code;
+};
+
+Blockly.Blocks['musicplay'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Groti muziką");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['musicplay'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'musicPlay();\n';
+  return code;
+};
+Blockly.Blocks['dance'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Šokis")
+        .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"]]), "sokiai");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['dance'] = function(block) {
+  var dropdown_sokiai = block.getFieldValue('sokiai');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'costumeChange('+dropdown_sokiai+');\n';
+  return code;
 };
