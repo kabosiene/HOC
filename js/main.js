27871a2842c868f7b38d6ad5328d76233d1643e2
Kristina <kristina.kabosiene@gmail.com>
2017-11-14 16:55:41 +0200

    add change to logo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+// })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+// ga('create', 'UA-70814234-1', 'auto');
+// ga('send', 'pageview');
+
 
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
@@ -21,3 +29,31 @@ var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimat
     // $(window).on('load', function() {
     //     $('#myModal').modal('show');
     // });
+
+    var currentCallback;
+
+// override default browser alert
+window.alert = function(msg, callback){
+  $('.message').text(msg);
+  $('.customAlert').css('animation', 'fadeIn 0.3s linear');
+  $('.customAlert').css('display', 'inline');
+  setTimeout(function(){
+    $('.customAlert').css('animation', 'none');
+  }, 300);
+  currentCallback = callback;
+}
+
+$(function(){
+  
+  // add listener for when our confirmation button is clicked
+    $('.confirmButton').click(function(){
+    $('.customAlert').css('animation', 'fadeOut 0.3s linear');
+    setTimeout(function(){
+     $('.customAlert').css('animation', 'none');
+        $('.customAlert').css('display', 'none');
+    }, 300);
+    currentCallback();
+  })
+
+  
+});
